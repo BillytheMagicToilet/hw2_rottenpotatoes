0a2eb22e21a1b9533b9d46ef3dbd09f663fb0e7a
BillytheMagicToilet <jack.skinner@colorado.edu>
2013-10-14 16:04:07 -0600

    Update movies_controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,7 +8,7 @@ class MoviesController < ApplicationController
 
   def index
     if params[:sort].nil? && params[:ratings].nil? &&
-        (!session[sort].nil? || !session[:ratings].nil?)
+        (!session[:sort].nil? || !session[:ratings].nil?)
     redirect_to movies_path(:sort => session[:sort], :ratings => session[:ratings])
     end
 
